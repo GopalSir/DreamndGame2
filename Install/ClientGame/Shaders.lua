d30c32e30b34f49774c44c9fa984658033d61c61
GopalSir <donkumar727@gmail.com>
2026-08-16 08:04:47 +0530

    lighting changes are there now. commiting to make way for robots

--- Install/ClientGame/Shaders.lua
+++ Install/ClientGame/Shaders.lua
@@ -29,7 +29,7 @@ defaultFragmentShader = [[
 #version 330 core
 
 uniform vec4 ambient_light;
-uniform vec3 light_position;
+uniform vec4 light_position;
 
 out vec4 fragColor;
 in vec4 vertexColor;
@@ -41,15 +41,55 @@ void main()
 	
 	
     //vec4 baseColor = vec4(0.4, 0.7, 0.2, 1.0);
-    fragColor = vertexColor * ambient_light; // Simple lighting calculation using vertex color and ambient light
+    fragColor = vec4(0.0, 0.0, 0.0, 1.0); // Start with black color
+    //fragColor = vertexColor * ambient_light; // Simple lighting calculation using vertex color and ambient light
 
-    vec3 lightDir = normalize(light_position - fragPos);
+    vec3 testLightPos = vec3(0, 20.0, 0.0); // Example light position
+    vec3 lightDir = normalize(light_position.xyz - fragPos);
     float diff = max(dot(normal, lightDir), 0.0);
     fragColor += vertexColor * diff; // Add diffuse lighting based on the normal and light direction
 
     
 	
 }
-]]}
+]],
+
+defaultLightSourceVertexShader = [[
+#version 330 core
+
+layout(location = 0) in vec3 aPos;
+
+
+
+uniform mat4 model_matrix;
+uniform mat4 view_matrix;
+uniform mat4 projection_matrix;
+
+uniform vec4 light_position;
+
+void main()
+{
+    //First we need to translate the light source cube to the light position. We can do this by modifying the model matrix to include a translation to the light position.
+    mat4 translationMatrix = mat4(1.0); // Identity matrix
+    translationMatrix[3][0] = light_position.x; // Set the x component of the translation
+    translationMatrix[3][1] = light_position.y; // Set the y component of the translation
+    translationMatrix[3][2] = light_position.z; // Set the z component of the translation
+
+        gl_Position = projection_matrix * view_matrix * translationMatrix * vec4(aPos, 1.0);
+}
+]],
+
+defaultLightSourceFragmentShader = [[
+#version 330 core
+out vec4 fragColor;
+
+void main()
+{
+    fragColor = vec4(1.0, 1.0, 1.0, 1.0); // Light source is rendered as white
+}
+
+]]
+
+}
 
 return shaders
